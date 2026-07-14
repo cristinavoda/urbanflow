a6976fab9bdce3be85411926ba460a518632b981
cristinavoda <cristinavoda483@gmail.com>
2026-07-14 22:34:20 +0200

    add '.env'

--- backend/index.php
+++ backend/index.php
@@ -2,28 +2,29 @@
 
 $allowedOrigins = [
     'http://localhost:5173',
-     'http://localhost:5174',
+    'http://localhost:5174',
+    'https://urbanflow3.netlify.app',
     'https://incandescent-sawine-798d34.netlify.app'
 ];
 
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+if (
+    isset($_SERVER['HTTP_ORIGIN']) &&
+    in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)
+) {
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
 }
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
 
-
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/bootstrap.php';
 
-use Core\Application;
-
-$app = new Application();
-
+$app = new Core\Application();
 $app->run();
