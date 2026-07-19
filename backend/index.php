78bca146df3d90b75f2fa813dca6b51a25b3b1fd
cristinavoda <cristinavoda483@gmail.com>
2026-07-19 19:22:04 +0200

    add 'api-2'

--- backend/index.php
+++ backend/index.php
@@ -2,11 +2,9 @@
 
 $allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:5174',
-    'https://urbanflow3.netlify.app',
-    
+     'http://localhost:5174',
+    'https://incandescent-sawine-798d34.netlify.app'
 ];
-
 if (
     isset($_SERVER['HTTP_ORIGIN']) &&
     in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)
@@ -18,13 +16,18 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
 
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/bootstrap.php';
 
-$app = new Core\Application();
+use Core\Application;
+
+$app = new Application();
+
 $app->run();
